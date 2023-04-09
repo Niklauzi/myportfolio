11d8e0fd1c71dce592fb73b3422e7af3dcc741ec
Niklauzi <ftpfavour@gmail.com>
2023-04-09 23:31:07 +0100

    added the projectCard, and implementing mailchimp

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,9 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Nav, Tab } from "react-bootstrap";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-
+import ProjectCard from "./ProjectCards";
+import colorSharp2 from "../assets/img/color-sharp2.png";
 
 
 const Projects = () => {
@@ -40,19 +41,48 @@ const Projects = () => {
     },
   ];
 
-
-    return(
-        <secction>
-            <Container>
-                <Row>
-                    <Col>
-                    <h2>Projects</h2>
-                    </Col>
-                </Row>
-            </Container>
-        </secction>
-
-    )
+    return (
+      <section className="project" id="projects">
+        <Container>
+          <Row>
+            <Col size={12}>
+              <h2>Projects</h2>
+              <p>KillTrack! Project</p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav
+                  variant="pills"
+                  className="nav-pills mb-5 justify-content-center align-items-center"
+                  id="pills-tab"
+                  defaultActiveKey="/home"
+                >
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Tab two</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Tab.Content >
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {projects.map((project, index) => {
+                        return <ProjectCard key={index} {...project} />;
+                      })}
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">Covalent</Tab.Pane>
+                  <Tab.Pane eventKey="third">bitsCrunch</Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </Col>
+          </Row>
+          <img className="background-image-right" src={colorSharp2} />
+        </Container>
+      </section>
+    );
 }
 
 
