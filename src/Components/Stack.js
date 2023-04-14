0d237562675ec459bcb41310e72b7751ca0625f4
Niklauzi <ftpfavour@gmail.com>
2023-04-14 23:38:35 +0100

    adding css to stack

--- src/Components/Stack.js
+++ src/Components/Stack.js
@@ -13,10 +13,21 @@ const Stack = () => {
             <Col size={12}>
               <h2>Stack</h2>
               <div className="stack">
-                <span><h3>HTML</h3></span>
-                <span><h3>BOOTSTRAP</h3></span>
-                <span><h3>JAVASCRIPT</h3></span>
-                <span><h3>REACT</h3></span>
+                <span>
+                  <h3>HTML</h3>
+                </span>
+                <span>
+                  <h3>CSS</h3>
+                </span>
+                <span>
+                  <h3>BOOTSTRAP</h3>
+                </span>
+                <span>
+                  <h3>JAVASCRIPT</h3>
+                </span>
+                <span>
+                  <h3>REACT</h3>
+                </span>
               </div>
             </Col>
           </Row>
