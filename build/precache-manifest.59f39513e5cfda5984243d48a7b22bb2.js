906a93fb62b48f2615dd29c94735886294429114
Niklauzi <ftpfavour@gmail.com>
2023-04-15 15:57:03 +0100

    running npm build on main branch

---
+++ build/precache-manifest.59f39513e5cfda5984243d48a7b22bb2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2e8c439b967300bcf4",
+    "url": "/myportfolio/static/css/main.a88c4d1b.chunk.css"
+  },
+  {
+    "revision": "7c2e8c439b967300bcf4",
+    "url": "/myportfolio/static/js/main.7c2e8c43.chunk.js"
+  },
+  {
+    "revision": "315327e41e41ef1b3c48",
+    "url": "/myportfolio/static/js/1.315327e4.chunk.js"
+  },
+  {
+    "revision": "672f55f6502b8ef05c17",
+    "url": "/myportfolio/static/css/2.ca5274a0.chunk.css"
+  },
+  {
+    "revision": "672f55f6502b8ef05c17",
+    "url": "/myportfolio/static/js/2.672f55f6.chunk.js"
+  },
+  {
+    "revision": "57540120e1a560eed6bd",
+    "url": "/myportfolio/static/js/runtime~main.57540120.js"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/myportfolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/myportfolio/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/myportfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/myportfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/myportfolio/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/myportfolio/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/myportfolio/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "33f3e6cecade5f96b59f85512ba29bfd",
+    "url": "/myportfolio/static/media/twitter.33f3e6ce.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/myportfolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/myportfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/myportfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/myportfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/myportfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/myportfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/myportfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/myportfolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/myportfolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/myportfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/myportfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/myportfolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/myportfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "d04c6dd3e387a6cfc71ecf9048228c5b",
+    "url": "/myportfolio/index.html"
+  }
+];
